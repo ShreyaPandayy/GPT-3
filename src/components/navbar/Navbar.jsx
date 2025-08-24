a5f69f2c3b9666662e86086231eb15b0c7cf6721
ShreyaPandayy <shreeyapandey456@gmail.com>
2025-08-25 03:44:55 +0530

    first

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
         <div className="gpt3__navbar-links_container">
           <p><a href="#home">Home</a></p>
           <p><a href="#wgpt3">What is?</a></p>
-          <p><a href="#automation">Services</a></p>
+          <p><a href="#possibility">Services</a></p>
           <p><a href="#features">What we do?</a></p>
           <p><a href="#blog">Library</a></p>
         </div>
