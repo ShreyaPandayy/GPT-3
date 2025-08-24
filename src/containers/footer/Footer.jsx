9ef4ffffb20286a430f9b23a4dab40e19f6a3fec
ShreyaPandayy <shreeyapandey456@gmail.com>
2025-08-25 05:07:23 +0530

    1

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -33,7 +33,7 @@ const Footer = () => (
       <div className="gpt3__footer-links_div">
         <h4>Get in touch</h4>
         <p>Global Team – Fully Remote</p>
-        <p>/p>
+        
         <p>info.eternadigital@gmail.com</p>
       </div>
     </div>
