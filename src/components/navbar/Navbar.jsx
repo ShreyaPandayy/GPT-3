0ca828a5db36fea5a0ca66451d1aaa4e24b2b560
ShreyaPandayy <shreeyapandey456@gmail.com>
2025-08-25 03:47:12 +0530

    1

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -31,8 +31,8 @@ const Navbar = () => {
           <div className="gpt3__navbar-menu_container scale-up-center">
             <div className="gpt3__navbar-menu_container-links">
               <p><a href="#home">Home</a></p>
-              <p><a href="#wgpt3">What is GPT3?</a></p>
-              <p><a href="#possibility">Open AI</a></p>
+              <p><a href="#wgpt3">What is?</a></p>
+              <p><a href="#possibility">Services</a></p>
               <p><a href="#features">What we do</a></p>
               <p><a href="#blog">Library</a></p>
             </div>
